85f04f45fc0d602d83dc29a90d3c1b27844755d9
BogdanKlimov11 <136115919+BogdanKlimov11@users.noreply.github.com>
2024-02-20 19:58:11 +0300

    Update Smooth_flashing.ino

--- Arduino_UNO/Smooth_flashing.ino
+++ Arduino_UNO/Smooth_flashing.ino
@@ -1,6 +1,9 @@
-int ledPin = 13; // Пин для встроенного светодиода (13 на Arduino UNO)
-int brightness = 0; // Начальная яркость
-int fadeAmount = 1; // Величина изменения яркости
+// Пин для встроенного светодиода (13 на Arduino UNO)
+int ledPin = 13;
+// Начальная яркость
+int brightness = 0;
+// Величина изменения яркости
+int fadeAmount = 1;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
@@ -11,13 +14,18 @@ void loop() {
 }
 
 void megay() {
-  digitalWrite(ledPin, HIGH); // Включаем светодиод
-  delay(brightness); // Ждем в течение времени, равного текущей яркости
-  digitalWrite(ledPin, LOW); // Выключаем светодиод
-  delay(30 - brightness); // Ждем оставшееся время
-  brightness = brightness + fadeAmount; // Изменяем яркость
-  if (brightness <= 0 || brightness >= 30) { // Если достигли минимальной или максимальной яркости, меняем направление изменения
+  // Включаем светодиод
+  digitalWrite(ledPin, HIGH);
+  // Ждем в течение времени, равного текущей яркости
+  delay(brightness);
+  // Выключаем светодиод
+  digitalWrite(ledPin, LOW);
+  // Ждем оставшееся время
+  delay(30 - brightness);
+  // Изменяем яркость
+  brightness = brightness + fadeAmount;
+  // Если достигли минимальной или максимальной яркости, меняем направление изменения
+  if (brightness <= 0 || brightness >= 30) {
     fadeAmount = -fadeAmount;
   }
-
 }
