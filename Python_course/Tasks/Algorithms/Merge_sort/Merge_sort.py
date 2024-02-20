4a3905eac651c73eabf6190e6d6f87a6640e5fa2
BogdanKlimov11 <136115919+BogdanKlimov11@users.noreply.github.com>
2024-02-20 20:17:11 +0300

    Add files via upload

---
+++ Python_course/Tasks/Algorithms/Merge_sort/Merge_sort.py
@@ -0,0 +1,30 @@
+# Объединяет два отсортированых массива
+def __merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
+
+# Сортировка слиянием
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return __merge(left, right)
+
