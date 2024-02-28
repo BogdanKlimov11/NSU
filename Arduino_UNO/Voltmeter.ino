94c80cabc9fa69dd854ffa0bc77d132121a80fb1
Bogdan Klimov <136115919+BogdanKlimov11@users.noreply.github.com>
2024-02-28 09:44:22 +0300

    Update Voltmeter.ino

--- Arduino_UNO/Voltmeter.ino
+++ Arduino_UNO/Voltmeter.ino
@@ -1,3 +1,10 @@
+// смещениен нуля
+float k1 = 0.02;
+// смещение опорного нуля
+float k2 = 5.15;
+// диапозон работы Arduino UNO
+float k = k2 - k1;
+
 void setup() {
   // инициализация последовательной связи
   Serial.begin(9600);
@@ -7,7 +14,7 @@ void loop() {
   // считываем значение с аналогового пина
   int sensorValue = analogRead(A0);
   // преобразуем значение в напряжение
-  float voltage = sensorValue * (5.0 / 1023.0);
+  float voltage = sensorValue * (k / 1023.0);
   // выводим значение в консоль
   Serial.print("Напряжение на батарейке: ");
   Serial.print(voltage);
