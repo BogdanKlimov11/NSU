2f14de3cca515f9e67d7a6943498281048a88356
Bogdan Klimov <136115919+BogdanKlimov11@users.noreply.github.com>
2024-03-19 21:43:45 +0300

    Delete MatLab_course directory

--- MatLab_course/Charges_and_multipoles/Exercise_5/point_charge_field.m
+++
@@ -1,9 +0,0 @@
-function E=point_charge_field(Q,x0,x)
-    if x0 == x 
-        E=[NaN NaN NaN];
-    else
-        R=[x(1)-x0(1),x(2)-x0(2),x(3)-x0(3)];
-        E=(Q/dot(R,R))*(R/norm(R));
-    end
-  %return E  
-end
