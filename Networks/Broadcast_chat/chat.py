ea948e7015f5489b63013164c8d5000830eaf6f9
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-09-02 17:43:46 +0300

    Add files via upload

---
+++ Networks/Broadcast_chat/chat.py
@@ -0,0 +1,31 @@
+import socket
+from threading import Thread
+import queue
+
+
+def receiver(socket, mes):
+    while True:
+        message, addr = socket.recvfrom(1024)
+        print('from:', addr,': ', message.decode())
+
+def sender(socket, mes):
+    while True:
+        str = input()
+        socket.sendto(str.encode(), ('255.255.255.255', 11719))
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind(('0.0.0.0',11719))
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
+mes = 'hello'
+
+th_receive = Thread(target=receiver, args=(s, mes))
+th_send = Thread(target=sender, args=(sock, mes))
+th_send.start()
+th_receive.start()
+
+
+
