0f9477a76e1c9203ed74500df83df662ae4743a2
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-10-15 15:48:08 +0700

    Update main.c

--- STM32/LED_control_with_button/Incomplete_circle/main.c
+++ STM32/LED_control_with_button/Incomplete_circle/main.c
@@ -23,15 +23,15 @@ void init(void){
 	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 	
-	GPIOC -> MODER &=~(GPIO_MODER_MODER6 | GPIO_MODER_MODER7 | GPIO_MODER_MODER8 | GPIO_MODER_MODER9);
-	GPIOC -> MODER |= (GPIO_MODER_MODER6_0| GPIO_MODER_MODER7_0|GPIO_MODER_MODER8_0| GPIO_MODER_MODER9_0 );
+	GPIOC -> MODER &= ~(GPIO_MODER_MODER6 | GPIO_MODER_MODER7 | GPIO_MODER_MODER8 | GPIO_MODER_MODER9);
+	GPIOC -> MODER |= (GPIO_MODER_MODER6_0| GPIO_MODER_MODER7_0|GPIO_MODER_MODER8_0| GPIO_MODER_MODER9_0);
 	
 	GPIOC->ODR |= GPIO_ODR_6;
 	
-	GPIOA->MODER &=~ GPIO_MODER_MODER0;
+	GPIOA->MODER &= ~GPIO_MODER_MODER0;
 	
 	SystemCoreClockUpdate(); // System_Config(SystemCoreClock)
-	SysTick->LOAD = SystemCoreClock/1000-1; // 1 ms
+	SysTick->LOAD = SystemCoreClock / 1000 - 1; // 1 ms
 	SysTick->CTRL = SysTick_CTRL_ENABLE_Msk | SysTick_CTRL_CLKSOURCE_Msk; 
 }
 
@@ -42,7 +42,7 @@ static uint8_t systick_flat_check(void){
 
 
 int delay(void){
-	if (systick_flat_check()==1){
+	if (systick_flat_check() == 1){
 		return(1);
 	}
 	else{
@@ -53,80 +53,80 @@ int delay(void){
 
 static void light(uint8_t state, struct buttons* buttons){
 	if (state){
-		if (buttons->first==1){
+		if (buttons->first == 1){
 			//GPIOC->ODR &= ~GPIO_ODR_6;
-			GPIOC->BRR=GPIO_BRR_BR_6;
+			GPIOC->BRR = GPIO_BRR_BR_6;
 			//GPIOC->ODR |= GPIO_ODR_8;
-			GPIOC->BSRR=GPIO_BSRR_BS_8;
-			buttons->first=0;
-			buttons->second=1;
+			GPIOC->BSRR = GPIO_BSRR_BS_8;
+			buttons->first = 0;
+			buttons->second = 1;
 			return;
 			
-			//GPIOC->BSRR=(state) ? CPIO_BSRR_BS_6 : GPIO_BSRR_BR_6;//tolko dla portov obchey naznacheny
+			//GPIOC->BSRR = state ? CPIO_BSRR_BS_6 : GPIO_BSRR_BR_6; // for general purpose ports only
     		}
-		if (buttons->second==1){
-			//GPIOC->ODR &=~GPIO_ODR_8;
-			GPIOC->BRR=GPIO_BRR_BR_8;
+		if (buttons->second == 1){
+			//GPIOC->ODR &= ~GPIO_ODR_8;
+			GPIOC->BRR = GPIO_BRR_BR_8;
 			//GPIOC->ODR |= GPIO_ODR_7; 
-			GPIOC->BSRR=GPIO_BSRR_BS_7;
-			buttons->second=0;
-			buttons->third=1;
+			GPIOC->BSRR = GPIO_BSRR_BS_7;
+			buttons->second = 0;
+			buttons->third = 1;
 			return;
 		}
-		if (buttons->third==1){
-			//GPIOC->ODR &=~GPIO_ODR_7;
-			GPIOC->BRR=GPIO_BRR_BR_7;
+		if (buttons->third == 1){
+			//GPIOC->ODR &= ~GPIO_ODR_7;
+			GPIOC->BRR = GPIO_BRR_BR_7;
 			//GPIOC->ODR |= GPIO_ODR_9; 
-			GPIOC->BSRR=GPIO_BSRR_BS_9;
-			buttons->third=0;
-			buttons->four=1;
+			GPIOC->BSRR = GPIO_BSRR_BS_9;
+			buttons->third = 0;
+			buttons->four = 1;
 			return;
 		}
-		if (buttons->four==1){
-			//GPIOC->ODR &=~GPIO_ODR_9;
-			GPIOC->BRR=GPIO_BRR_BR_9;
+		if (buttons->four == 1){
+			//GPIOC->ODR &= ~GPIO_ODR_9;
+			GPIOC->BRR = GPIO_BRR_BR_9;
 			//GPIOC->ODR |= GPIO_ODR_6; 
-			GPIOC->BSRR=GPIO_BSRR_BS_6;
-			buttons->four=0;
-			buttons->first=1;
+			GPIOC->BSRR = GPIO_BSRR_BS_6;
+			buttons->four = 0;
+			buttons->first = 1;
 			return;
 		}
 	}
 	else{
-		if (buttons->first==1){
-			//GPIOC->ODR &=~GPIO_ODR_6;
-			GPIOC->BRR=GPIO_BRR_BR_6;
+		if (buttons->first == 1){
+			//GPIOC->ODR &= ~GPIO_ODR_6;
+			GPIOC->BRR = GPIO_BRR_BR_6;
 			//GPIOC->ODR |= GPIO_ODR_9; 
-			GPIOC->BSRR=GPIO_BSRR_BS_9;
-			buttons->first=0;
-			buttons->four=1;
+			GPIOC->BSRR = GPIO_BSRR_BS_9;
+			buttons->first = 0;
+			buttons->four = 1;
 			return;
 		}
-		if (buttons->second==1){
-			//GPIOC->ODR &=~GPIO_ODR_8;
-			GPIOC->BRR=GPIO_BRR_BR_8;
+		if (buttons->second == 1){
+			//GPIOC->ODR &= ~GPIO_ODR_8;
+			GPIOC->BRR = GPIO_BRR_BR_8;
 			//GPIOC->ODR |= GPIO_ODR_6; 
-			GPIOC->BSRR=GPIO_BSRR_BS_6;
-			buttons->second=0;
-			buttons->first=1;
+			GPIOC->BSRR = GPIO_BSRR_BS_6;
+			buttons->second = 0;
+			buttons->first = 1;
 			return;
 		}
-		if (buttons->third==1){
-			//GPIOC->ODR &=~GPIO_ODR_7;
-			GPIOC->BRR=GPIO_BRR_BR_7;
+		if (buttons->third == 1){
+			//GPIOC->ODR &= ~GPIO_ODR_7;
+			GPIOC->BRR = GPIO_BRR_BR_7;
 			//GPIOC->ODR |= GPIO_ODR_8; 
-			GPIOC->BSRR=GPIO_BSRR_BS_8;
-			buttons->third=0;
-			buttons->second=1;
+			GPIOC->BSRR = GPIO_BSRR_BS_8;
+			buttons->third = 0;
+			buttons->second = 1;
 			return;
 		}
-		if (buttons->four==1){
-			//GPIOC->ODR &=~GPIO_ODR_9;
-			GPIOC->BRR=GPIO_BRR_BR_9;
+		if (buttons->four == 1){
+			//GPIOC->ODR &= ~GPIO_ODR_9;
+			GPIOC->BRR = GPIO_BRR_BR_9;
 			//GPIOC->ODR |= GPIO_ODR_7; 
-			GPIOC->BSRR=GPIO_BSRR_BS_7;
-			buttons->four=0;
-			buttons->third=1;
+			GPIOC->BSRR = GPIO_BSRR_BS_7;
+			buttons->four = 0;
+			buttons->third = 1;
 			return;
 		}
 	}
@@ -138,7 +138,7 @@ static uint8_t btn_stable_prev_state = 0;
 static uint8_t _edge_detected = 0;
 static uint8_t _pressed = 0;
 static uint8_t _unpressed = 0;
-static uint32_t BTN_T = 5; //5ms
+static uint32_t BTN_T = 5; // 5 ms
 static uint8_t state = 0;
 
 
@@ -158,11 +158,11 @@ static uint8_t btn_filter(){
 			if (btn_stable_curr_state != btn_stable_prev_state){
 				btn_stable_prev_state = btn_stable_curr_state;
 				//_edge_detected = 1;
-				if (btn_stable_prev_state==0){
+				if (btn_stable_prev_state == 0){
 					state = !state;
 					_unpressed = 1;
 				}
-				/*if ( btn_stable_curr_state ){
+				/*if(btn_stable_curr_state){
 					_pressed = 1;
 				}
 				else{
@@ -203,30 +203,30 @@ int main(void){
 	but.second = 0;
 	but.third = 0;
 	but.four = 0;
-	int light_delay=0;
+	int light_delay = 0;
 	
 	while(1){
-		/*if (((GPIO_IDR_0 & GPIOA->IDR)!=0) & (state==0)){
+		/*if(((GPIO_IDR_0 & GPIOA->IDR) != 0) & (state == 0)){
 			state = 1;
 		}
-		else if (((GPIO_IDR_0 & GPIOA->IDR)!=0) & (state==1)){
+		else if(((GPIO_IDR_0 & GPIOA->IDR) != 0) & (state == 1)){
 			state = 0;
 		}*/
 		
 		if (systick_flat_check()){
-			light_delay=light_delay+1;
+			light_delay = light_delay + 1;
 			btn_filter();
 		}
 		
 		//state = btn_state(); // tmp - nash state
 		if (btn_pressed()){
-			_pressed=0;
+			_pressed = 0;
 		}
 		
-		int s_witch=delay();
-		if (s_witch==1 && light_delay>=200){
+		int s_witch = delay();
+		if (s_witch == 1 && light_delay >= 200){
 			light(state, &but);
-			light_delay=0;
+			light_delay = 0;
 		}
 	}
 }
