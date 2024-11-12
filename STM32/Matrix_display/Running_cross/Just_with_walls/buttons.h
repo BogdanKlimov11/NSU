2015552d63ca3668b2ea292af984b44c2e5f1e47
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-11-12 16:02:56 +0700

    Add files via upload

---
+++ STM32/Matrix_display/Running_cross/Just_with_walls/buttons.h
@@ -0,0 +1,27 @@
+#ifndef _BUTTONS_H_
+#define _BUTTONS_H_
+
+#include "stm32f0xx.h"
+#include <stdlib.h>
+
+typedef uint8_t (*GroundBtnRead)(void);
+
+typedef struct ConfButton{
+    uint32_t listenRange;
+    uint32_t countWithoutChange;
+		uint8_t currStableState;
+    uint8_t currInstantState;
+    uint8_t prevStableState;
+    uint8_t prevInstantState;
+		GroundBtnRead groundBtnRead;
+} ConfButton;
+
+ConfButton InitButton(GroundBtnRead groundBtnRead);
+//uint8_t ButtonReadCurrInstantState(void);
+void HandlerButton(ConfButton* button);
+uint8_t PrevStableState(ConfButton* button);
+uint8_t CurrStableState(ConfButton* button);
+uint8_t PushButtListnr(ConfButton* button);
+uint8_t ReleasingButtListnr(ConfButton* button);
+
+#endif
