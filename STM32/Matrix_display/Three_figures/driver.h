98205d08236c3e657a09bdaa4044f4c90675390d
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-11-26 15:39:26 +0700

    Update driver.h

--- STM32/Matrix_display/Three_figures/driver.h
+++ STM32/Matrix_display/Three_figures/driver.h
@@ -5,12 +5,12 @@
 #include "object.h"
 
 typedef struct ScreenScene{
-		uint8_t alreadyModif;
-    uint8_t currDrawedFrame;
-		uint8_t currModifiedFrame;
-    uint8_t currDrawedColumn;
-		uint16_t frame[2][8];
-		uint16_t emptyFrame[8];
+	uint8_t alreadyModif;
+	uint8_t currDrawedFrame;
+	uint8_t currModifiedFrame;
+	uint8_t currDrawedColumn;
+	uint16_t frame[2][8];
+	uint16_t emptyFrame[8];
 }ScreenScene;
 
 ScreenScene InitScene(void);
