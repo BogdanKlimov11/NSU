13fe70821eeb6cac65f8a57c172af60a080d6e12
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-11-26 15:37:51 +0700

    Update button.c

--- STM32/Matrix_display/Three_figures/button.c
+++ STM32/Matrix_display/Three_figures/button.c
@@ -15,46 +15,46 @@ ConfButton InitButton(GroundBtnRead groundBtnRead){
 }
 
 //uint8_t ButtonReadCurrInstantState(){
-  //  return (GPIO_IDR_0 & GPIOA->IDR) ? 1 : 0;
+//	return (GPIO_IDR_0 & GPIOA->IDR) ? 1 : 0;
 //}
 
 void HandlerButton(ConfButton* button){
 	if (button!=NULL){
 		button->prevStableState = button->currStableState;
-    button->currInstantState = button->groundBtnRead();
-
-    if (button->prevInstantState != button->currInstantState){
-        button->countWithoutChange = 0;
-    }
-    else{
-        if (button->countWithoutChange >= button->listenRange){
-					button->prevStableState = button->currStableState;
-					button->currStableState = button->currInstantState;
-					if ((button->currStableState == 1) && (button->prevStableState == 0)){
-							button->pushBtn = 1;
-						}
-						if ((button->currStableState == 0) && (button->prevStableState == 1)){
-							button->releaseBtn = 1;
-						}
-        }
-        else{
-            button->countWithoutChange++;
-        }  
-    }
-    button->prevInstantState = button->currInstantState;
+		button->currInstantState = button->groundBtnRead();
+		
+		if (button->prevInstantState != button->currInstantState){
+			button->countWithoutChange = 0;
+		}
+		else{
+			if (button->countWithoutChange >= button->listenRange){
+				button->prevStableState = button->currStableState;
+				button->currStableState = button->currInstantState;
+				if ((button->currStableState == 1) && (button->prevStableState == 0)){
+					button->pushBtn = 1;
+					}
+					if ((button->currStableState == 0) && (button->prevStableState == 1)){
+						button->releaseBtn = 1;
+					}
+			}
+			else{
+				button->countWithoutChange++;
+			}
+		}
+   	button->prevInstantState = button->currInstantState;
 	}
 }
 
 uint8_t CurrStableState(ConfButton* button){
 	if (button!=NULL){
-    return button->currStableState;
+		return button->currStableState;
 	}
 	return 0;
 }
 
 uint8_t PrevStableState(ConfButton* button){
 	if (button!=NULL){
-    return button->prevStableState;
+		return button->prevStableState;
 	}
 	return 0;
 }
