449445c0289436c627de4925288d2abc08f7f3ab
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-12-17 00:51:19 +0700

    Update test_camera.py

--- Robotics/Python_code/test_camera.py
+++ Robotics/Python_code/test_camera.py
@@ -2,45 +2,50 @@ import cv2
 import numpy
 from math import *
 
+# Матрица камеры (фокусное расстояние и координаты главной точки)
 mtx = numpy.matrix([[920.0, 0.0, 650.0], [0.0, 930.0, 320.0], [0.0, 0.0, 1.0]])
+# Коэффициенты искажения камеры
 dist = numpy.matrix([[0.06], [-0.1], [-0.006], [0.009], [0]])
 
+# Открытие видеопотока с камеры
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # Установка ширины изображения
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)  # Установка высоты изображения
 
 while True:
-    ret, img = cap.read()
-    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_1000)
-    # parameters = cv2.aruco.DetectorParameters()
+    ret, img = cap.read()  # Захват одного кадра
+    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_1000)  # Загрузка словаря маркеров ArUco
     print("1")
+    
+    # Обнаружение маркеров ArUco на изображении
     (corners, ids, rejected) = cv2.aruco.detectMarkers(img, dictionary=arucoDict)
-    # print("2")
-    if len(corners) > 0:
+    if len(corners) > 0:  # Если маркеры найдены
+        # Отображение обнаруженных маркеров
         cv2.aruco.drawDetectedMarkers(img, corners, ids)
-        # print("3")
         
+        # Оценка положения и ориентации маркеров
         rvec, tvec, _ =  cv2.aruco.estimatePoseSingleMarkers(corners, 0.12, mtx, dist)
-        print("Detected: ", tvec)
+        print("Detected: ", tvec)  # Печать трансляции маркера
 
+        # Преобразование в матрицу вращения с помощью Rodrigues
         R, jac = cv2.Rodrigues(rvec, None, None)
-        # print("Rodrogues: ", matrix)
-        # b_angle = asin(matrix[0][2])
-        # g_angle = asin(matrix[0][1] / (-cos(asin2(matrix[0][2]))))
-        # g_angle = g_angle + pi / 2
 
-        # beta = -np.arcsin(R[2,0])
-        # alpha = np.arctan2(R[2,1] / np.cos(beta),R[2,2] / np.cos(beta))
+        # Вычисление угла ориентации маркера относительно оси Z
         g_angle = atan2(R[1,0], R[0,0])
 
+        # Печать угла ориентации и координат маркера
         print("Gamma: ", g_angle, tvec[0][0][0], tvec[0][0][1])
+
+        # Отображение осей на изображении для маркера
         cv2.drawFrameAxes(img, mtx, dist, rvec, tvec, length=0.1)
-    
+
+    # Отображение изображения с обнаруженными маркерами
     cv2.imshow('output', img)
 
-    key = cv2.waitKey(10)
-    # if key == 27:
+    key = cv2.waitKey(10)  # Ожидание 10 мс для нажатия клавиши
+    # if key == 27:  # Прерывание цикла по нажатию клавиши ESC
     #     break
 
+# Закрытие видеопотока и окон OpenCV
 cap.release()
 cv2.destroyAllWindows()
