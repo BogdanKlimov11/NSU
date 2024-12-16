3402a9d5b5431d284e998043fe51f137290eefc1
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2024-12-17 00:48:52 +0700

    Update My_sendVel.py

--- Robotics/Python_code/My_sendVel.py
+++ Robotics/Python_code/My_sendVel.py
@@ -5,11 +5,13 @@ import struct
 
 class My_driver:
     def __init__(self) -> None:
+        # Подписка на топик для получения команд скорости
         rospy.Subscriber('my_velocity', Twist, self.callback)
+        # Открытие последовательного порта для связи с устройством
         self.ser = Serial('/dev/ttyUSB0', 115200)
     
-
     def callback(self, data):
+        # Упаковка данных о скорости в бинарный формат и отправка по порту
         vel = struct.pack('ff',data.linear.x, data.angular.z)
         self.ser.write(vel)
         print('Velocity: ', vel)
