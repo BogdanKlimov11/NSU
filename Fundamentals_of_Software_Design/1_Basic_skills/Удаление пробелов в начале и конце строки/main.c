1b0359bbd51d772db26529af55e12fda047fa3e6
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-01-28 01:10:24 +0700

    Create main.c

---
+++ Fundamentals_of_Software_Design/1_Basic_skills/Удаление пробелов в начале и конце строки/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+void strtrim(char *string) 
+{
+    if (!string || *string == '\0') return;
+
+    // Указатель на начало строки без начальных пробелов
+    char *start = string;
+    while (isspace((unsigned char)*start)) {
+        start++;
+    }
+
+    // Указатель на конец строки без конечных пробелов
+    char *end = string + strlen(string) - 1;
+    while (end > start && isspace((unsigned char)*end)) {
+        *end = '\0'; // Обнуляем символы пробелов
+        end--;
+    }
+
+    // Сдвигаем оставшуюся часть строки к началу
+    if (start != string) {
+        memmove(string, start, end - start + 2);
+    }
+}
+
+int main() 
+{
+    char buf[] = "   abc   ";
+    printf("<%s>\n", buf); // <   abc   >
+    strtrim(buf);
+    printf("<%s>\n", buf); // <abc>
+    return 0;
+}
