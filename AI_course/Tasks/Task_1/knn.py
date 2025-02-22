5e6872f814e4a456335a4cd2adabf8e38c5d207d
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-02-23 00:14:05 +0700

    Update knn.py

--- AI_course/Tasks/Task_1/knn.py
+++ AI_course/Tasks/Task_1/knn.py
@@ -8,16 +8,16 @@ class KNN:
     def __init__(self, k=1):
         self.k = k
     
-
+    
     def fit(self, X, y):
         self.train_X = X
         self.train_y = y
     
-
+    
     def predict(self, X, num_loops=0):
         '''
         Uses the KNN model to predict clases for the data samples provided
-        
+
         Arguments:
         X, np array (num_samples, num_features) - samples to run
            through the model
@@ -39,7 +39,7 @@ class KNN:
         else:
             return self.predict_labels_multiclass(dists)
     
-
+    
     def compute_distances_two_loops(self, X):
         '''
         Computes L1 distance from every sample of X to every training sample
@@ -62,7 +62,7 @@ class KNN:
                 pass
         return dists
     
-
+    
     def compute_distances_one_loop(self, X):
         '''
         Computes L1 distance from every sample of X to every training sample
@@ -85,7 +85,7 @@ class KNN:
             pass
         return dists
     
-
+    
     def compute_distances_no_loops(self, X):
         '''
         Computes L1 distance from every sample of X to every training sample
@@ -107,7 +107,7 @@ class KNN:
         pass
         return dists
     
-
+    
     def predict_labels_binary(self, dists):
         '''
         Returns model predictions for binary classification case
@@ -115,7 +115,7 @@ class KNN:
         Arguments:
         dists, np array (num_test_samples, num_train_samples) - array
            with distances between each test and each train sample
-        
+
         Returns:
         pred, np array of bool (num_test_samples) - binary predictions 
            for every test sample
@@ -131,7 +131,7 @@ class KNN:
             pass
         return pred
     
-
+    
     def predict_labels_multiclass(self, dists):
         '''
         Returns model predictions for multi-class classification case
@@ -139,7 +139,7 @@ class KNN:
         Arguments:
         dists, np array (num_test_samples, num_train_samples) - array
            with distances between each test and each train sample
-        
+
         Returns:
         pred, np array of int (num_test_samples) - predicted class index 
            for every test sample
