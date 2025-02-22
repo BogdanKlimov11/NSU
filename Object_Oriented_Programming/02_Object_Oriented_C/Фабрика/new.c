296b0a48ea1a7b2d46330160064782e5d27e95e6
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-02-23 04:25:40 +0700

    Create new.c

---
+++ Object_Oriented_Programming/02_Object_Oriented_C/Фабрика/new.c
@@ -0,0 +1,31 @@
+#include "new.h"
+
+void* new(const void* _class, ...) {
+    const struct Class* class = _class;
+    void* p = calloc(1, class->size);
+    assert(p);
+    *(const struct Class**)p = class;
+
+    if (class->ctor) {
+        va_list ap;
+        va_start(ap, _class);
+        p = class->ctor(p, &ap);
+        va_end(ap);
+    }
+
+    return p;
+}
+
+void delete(void* self) {
+    const struct Class** cp = self;
+    if ((self) && (*cp) && ((*cp)->dtor)) {
+        self = (*cp)->dtor(self);
+    }
+    free(self);
+}
+
+void draw(const void *self) {
+    const struct Class * const *cp = self;
+    assert(self && *cp && (*cp)->draw);
+    (*cp)->draw(self);
+}
