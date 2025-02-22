973f1fbcdb2ed28938b4dc8301a2a4edfd88dc14
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-02-23 05:13:46 +0700

    Update main.cpp

--- Object_Oriented_Programming/03_Simple_classes/Простой класс String/main.cpp
+++ Object_Oriented_Programming/03_Simple_classes/Простой класс String/main.cpp
@@ -12,12 +12,12 @@ int main() {
     String str5(str2);
     String str6(str3, 2, 2);
     cout << str2.data() << " " << str3.data() << " " << 
-            str4.data() << " " << str5.data() << " " << str6.data() << " " << endl;
+        str4.data() << " " << str5.data() << " " << str6.data() << " " << endl;
     cout << str2.size() << " " << str2.capacity() << " " <<
-            str2.at(2) << " " << str2[2] << " " << str2.back() << " " <<
-            str2.front() << " " << str2.find('b') << " " <<
-            str2.compare("abcd") << " " << str2.countRef() << " " <<
-            str2.empty() << " " << str2.substr(1, 2).data() << endl;
+        str2.at(2) << " " << str2[2] << " " << str2.back() << " " <<
+        str2.front() << " " << str2.find('b') << " " <<
+        str2.compare("abcd") << " " << str2.countRef() << " " <<
+        str2.empty() << " " << str2.substr(1, 2).data() << endl;
     str2.clear();
     str2.insert(0, "meMememe");
     cout << str2.data() << " " << endl;
