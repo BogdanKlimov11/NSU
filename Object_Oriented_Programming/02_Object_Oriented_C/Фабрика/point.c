544fb19a3badc8b596e5037b31c3d81b90269a07
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-02-23 05:10:18 +0700

    Update point.c

--- Object_Oriented_Programming/02_Object_Oriented_C/Фабрика/point.c
+++ Object_Oriented_Programming/02_Object_Oriented_C/Фабрика/point.c
@@ -1,23 +1,23 @@
 #include "point.h"
 
 static void *Point_ctor(void *_self, va_list *app) {
-	struct Point *self = _self;
-	self->x = va_arg(*app, int);
-	self->y = va_arg(*app, int);
+    struct Point *self = _self;
+    self->x = va_arg(*app, int);
+    self->y = va_arg(*app, int);
 
-	return self;
+    return self;
 }
 
 static void Point_draw(const void *_self) {
-	const struct Point *self = _self;
-	printf("point at (%d,%d)\n", self->x, self->y);
+    const struct Point *self = _self;
+    printf("point at (%d,%d)\n", self->x, self->y);
 }
 
 static const struct Class _Point = {
-	sizeof(struct Point),
-	Point_ctor,
-	0,
-	Point_draw
+    sizeof(struct Point),
+    Point_ctor,
+    0,
+    Point_draw
 };
 
 const void *Point = &_Point;
