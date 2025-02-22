6f1c46251704ddbcb4ff433f0d7045e351df52ac
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-02-23 05:08:56 +0700

    Update rectangle.c

--- Object_Oriented_Programming/02_Object_Oriented_C/Фабрика/rectangle.c
+++ Object_Oriented_Programming/02_Object_Oriented_C/Фабрика/rectangle.c
@@ -1,7 +1,6 @@
 #include "rectangle.h"
 
-static void *Rectangle_ctor(void *_self, va_list *app) 
-{
+static void *Rectangle_ctor(void *_self, va_list *app) {
 	struct Rectangle *self = _self;
 	((const struct Class *)Point)->ctor(&self->first, app);
 	((const struct Class *)Point)->ctor(&self->second, app);
@@ -12,14 +11,12 @@ static void *Rectangle_ctor(void *_self, va_list *app)
 #define x(p) (((const struct Point *)(p)) -> x)
 #define y(p) (((const struct Point *)(p)) -> y)
 
-static void Rectangle_draw(const void * _self)
-{
+static void Rectangle_draw(const void * _self) {
 	const struct Rectangle *self = _self;
 	printf("rectangle at (%d,%d),(%d,%d)\n", x(&self->first), y(&self->first), x(&self->second), y(&self->second));
 }
 
-static const struct Class _Rectangle = 
-{
+static const struct Class _Rectangle = {
 	sizeof(struct Rectangle),
 	Rectangle_ctor,
 	0,
