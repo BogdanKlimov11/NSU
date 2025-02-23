9f13541d57fdee3da8456888a2c80567134fdae2
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-02-24 00:18:48 +0700

    Update writer.hpp

--- Object_Oriented_Programming/04_Data_structures/Построение минимального остовного дерева/writer.hpp
+++ Object_Oriented_Programming/04_Data_structures/Построение минимального остовного дерева/writer.hpp
@@ -3,10 +3,10 @@
 #include "graph.hpp"
 
 class Writer {
-    private:
-        std::ostream& outputStream;
-    
-    public:
-        Writer(std::ostream& os) : outputStream(os) {}
-        void writeMSTVector(const std::vector<std::pair<double, edge>>& mst);
+private:
+    std::ostream& outputStream;
+
+public:
+    Writer(std::ostream& os) : outputStream(os) {}
+    void writeMSTVector(const std::vector<std::pair<double, edge>>& mst);
 };
