12e81cd45661314ec1065d7affa965cb3cd59935
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-02-24 00:17:39 +0700

    Update disjoint_set.hpp

--- Object_Oriented_Programming/04_Data_structures/Построение минимального остовного дерева/disjoint_set.hpp
+++ Object_Oriented_Programming/04_Data_structures/Построение минимального остовного дерева/disjoint_set.hpp
@@ -5,14 +5,14 @@
 #include <algorithm>
 
 class DisjointSet final {
-    private:
-        std::vector<size_t> parent;
-        std::vector<size_t> rank;
-        size_t size;
-    
-    public:
-        DisjointSet(size_t n);
-        ~DisjointSet() = default;
-        size_t find(size_t u);
-        void setUnion(size_t v1, size_t v2);
+private:
+    std::vector<size_t> parent;
+    std::vector<size_t> rank;
+    size_t size;
+
+public:
+    DisjointSet(size_t n);
+    ~DisjointSet() = default;
+    size_t find(size_t u);
+    void setUnion(size_t v1, size_t v2);
 };
