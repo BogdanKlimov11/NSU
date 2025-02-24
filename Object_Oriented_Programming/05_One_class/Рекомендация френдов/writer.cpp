63781b8935acdd050bf7e7acbe32555794b99c0f
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-02-24 18:33:56 +0700

    Create writer.cpp

---
+++ Object_Oriented_Programming/05_One_class/Рекомендация френдов/writer.cpp
@@ -0,0 +1,9 @@
+#include "writer.h"
+
+void Writer::writeRecommendedFriends(const std::vector<std::string>& friends) {
+    if (friends.empty()) return;
+    os << "Recommendation for " << friends[0] << '\n';
+    for (size_t i = 1; i < friends.size(); i++) {
+        os << friends[i] << '\n';
+    }
+}
