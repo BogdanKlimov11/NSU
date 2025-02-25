4c1c64f02e098da4b8199f2228e6b8c791652c91
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-02-26 01:40:02 +0700

    Update Animate.m

--- MatLab_course/Introduction/Lissajous/Animate.m
+++ MatLab_course/Introduction/Lissajous/Animate.m
@@ -1,20 +1,34 @@
 function [t] = animate(t, dt, h, ho, ht, hs)
-[a1,a2,w1,w2,fi1,fi2] = inputparam();
+% Анимация движения с использованием входных параметров.
+% Обновляет графику с учётом времени и условий остановки.
+
+[a1, a2, w1, w2, fi1, fi2] = inputparam();  % Получаем параметры для анимации
+
 global stop;
-stoplabel = ['stop = ',num2str(stop)];
-set(hs,'String',stoplabel);
+
+% Обновление метки о состоянии остановки
+stoplabel = ['stop = ', num2str(stop)];
+set(hs, 'String', stoplabel);
+
 while(stop)
-    t = t + dt;
-    timelabel = ['t = ',num2str(t,'%.2f'),' s'];
-    set(ht,'String',timelabel);
-    x = a1*cos(w1*t + fi1);
-    y = a2*sin(w2*t + fi2);
-    addpoints(h,x,y);
-    clearpoints(ho);
-    addpoints(ho,x,y);
-%    pause(0.02)
-    drawnow
+    t = t + dt;  % Обновляем время
+    timelabel = ['t = ', num2str(t, '%.2f'), ' s'];
+    set(ht, 'String', timelabel);  % Обновляем метку времени
+
+    % Вычисляем новые координаты
+    x = a1 * cos(w1 * t + fi1);
+    y = a2 * sin(w2 * t + fi2);
+
+    % Обновляем анимацию
+    addpoints(h, x, y);
+    clearpoints(ho);  % Очищаем предыдущие точки
+    addpoints(ho, x, y);  % Добавляем новые точки
+
+    drawnow  % Обновление графики в реальном времени
 end
-stoplabel = ['stop = ',num2str(stop)];
-set(hs,'String',stoplabel);
+
+% Обновляем метку состояния остановки после завершения цикла
+stoplabel = ['stop = ', num2str(stop)];
+set(hs, 'String', stoplabel);
+
 end
