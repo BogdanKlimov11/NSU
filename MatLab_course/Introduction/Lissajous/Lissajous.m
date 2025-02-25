059a69dfee95cc9c1b5b35b49fa833134d6b0fb1
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-02-26 01:41:29 +0700

    Update Lissajous.m

--- MatLab_course/Introduction/Lissajous/Lissajous.m
+++ MatLab_course/Introduction/Lissajous/Lissajous.m
@@ -1,24 +1,43 @@
 clear;
-[a1,a2,w1,w2,fi1,fi2] = inputparam();
+
+% Задание параметров Лиссажу
+[a1, a2, w1, w2, fi1, fi2] = inputparam();
+
+% Инициализация времени и шага
 t = 0;
 dt = 0.03;
+
+% Глобальная переменная для остановки анимации
 global stop;
 
-x = a1*cos(w1*t + fi1);
-y = a2*sin(w2*t + fi2);
-h = animatedline(x,y,'Color','black');
-ho = animatedline(x,y,'Color','r','Marker','*','MarkerSize',5);
-axis([-a1 a1 -a2 a2],'equal');
+% Начальные координаты для анимации
+x = a1 * cos(w1 * t + fi1);
+y = a2 * sin(w2 * t + fi2);
+
+% Создание анимационных линий
+h = animatedline(x, y, 'Color', 'black');
+ho = animatedline(x, y, 'Color', 'r', 'Marker', '*', 'MarkerSize', 5);
+
+% Установка осей и границ
+axis([-a1 a1 -a2 a2], 'equal');
 title('Lissajous');
 grid on;
 xlabel('X');
 ylabel('Y');
+
+% Инициализация переменной остановки
 stop = 1;
-timelabel = ['t = ',num2str(t,'%.2f'),' s'];
-stoplabel = ['stop = ',num2str(stop)];
-ht = text(-a1,a2*1.05,timelabel);
-hs = text(a1,a2*1.05,stoplabel);
-set(gca,'ButtonDownFcn','stop = mod(stop+1,2); [t] = animate(t, dt, h, ho, ht, hs);')
 
+% Текущие метки времени и состояния остановки
+timelabel = ['t = ', num2str(t, '%.2f'), ' s'];
+stoplabel = ['stop = ', num2str(stop)];
+
+% Отображение текста на графике
+ht = text(-a1, a2 * 1.05, timelabel);
+hs = text(a1, a2 * 1.05, stoplabel);
+
+% Установка обработчика нажатия для переключения состояния анимации
+set(gca, 'ButtonDownFcn', 'stop = mod(stop + 1, 2); [t] = animate(t, dt, h, ho, ht, hs);')
+
+% Запуск анимации
 [t] = animate(t, dt, h, ho, ht, hs);
-
