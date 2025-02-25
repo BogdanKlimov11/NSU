f39d73e85585a7a0949c6b8b35096f902cfe16cc
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-02-26 00:22:25 +0700

    Update Field1.m

--- MatLab_course/Field/Field1.m
+++ MatLab_course/Field/Field1.m
@@ -8,9 +8,9 @@ set(0,'units','pixels'); res = get(0,'screensize');
 resX = res(3); resY = res(4);
 set(gcf,'position', [resX/2-resX/6, 0, resX/3, resY])
 subplot (2, 1, 1);
-h = animatedline(x, y, 'color', 'k'); %Траектория
-hp = animatedline(x, y, 'color', 'b', 'marker', 'o', 'markersize', 5); %Планета
-hS = line(0, 0, 'color', 'r', 'marker', 'pentagram', 'markersize', 7); %Центр поля
+h = animatedline(x, y, 'color', 'k'); % РўСЂР°РµРєС‚РѕСЂРёСЏ
+hp = animatedline(x, y, 'color', 'b', 'marker', 'o', 'markersize', 5); % РџР»Р°РЅРµС‚Р°
+hS = line(0, 0, 'color', 'r', 'marker', 'pentagram', 'markersize', 7); % Р¦РµРЅС‚СЂ РїРѕР»СЏ
 h_extr = animatedline(x, y, 'linestyle', 'none', 'color', 'm', 'marker', 'square', 'markersize', 10);
 clearpoints(h_extr);
 g = 2; axis([-g g -g g]); axis square; box on;
@@ -29,9 +29,9 @@ while ishghandle(h)
 clearpoints(hp); clearpoints(hEo);
 r2 = r1;
 r1 = r;
-R = R + V*dt; %Радиус-вектор
+R = R + V*dt; % Р Р°РґРёСѓСЃ-РІРµРєС‚РѕСЂ
 r = sqrt(R*R');
-A = -alpha*R/r^3; %Ускорение
+A = -alpha*R/r^3; % РЈСЃРєРѕСЂРµРЅРёРµ
 V = V + A*dt; x = R(1); y = R(2);
 E = V(1)^2/2 + V(2)^2/2 - alpha/r;
 if (r2 - r1)*(r1 - r) < 0
