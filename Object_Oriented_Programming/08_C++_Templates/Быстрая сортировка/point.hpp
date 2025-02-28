9c909c1799c5f290664147f0c4a1bee3384dc9ce
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-02-28 23:34:00 +0700

    Create point.hpp

---
+++ Object_Oriented_Programming/08_C++_Templates/Быстрая сортировка/point.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+template <typename T>
+class Point {
+private:
+    T x_;
+
+public:
+    Point(const T& x) : x_(x) {}
+    T x() const { return x_; }
+};
+
+template <typename T>
+bool operator<(const Point<T>& p1, const Point<T>& p2) { return p1.x() < p2.x(); }
+
+template <typename T>
+bool operator>(const Point<T>& p1, const Point<T>& p2) { return p1.x() > p2.x(); }
