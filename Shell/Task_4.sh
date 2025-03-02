47b0e3b4d6d13b29538f63afd434eb82cccbccc0
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-03-02 22:11:06 +0700

    Update Task_4.sh

--- Shell/Task_4.sh
+++ Shell/Task_4.sh
@@ -1,3 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-find $1 -type f,d | sed 's/.*\///' | while read c1; do echo "${#c1}/$c1" ; done | sort -n -r | head -1 | sed 's/.*\///'
+# Проверяем, был ли передан каталог в качестве аргумента
+if [ -z "$1" ]; then
+  echo "Ошибка: не указан каталог."
+  exit 1
+fi
+
+# Ищем файл или каталог с самым длинным именем
+find "$1" -type f -or -type d | while read -r file; do
+  basename "$file"
+done | awk '{ if (length($0) > max) { max=length($0); name=$0 } } END { print name }'
