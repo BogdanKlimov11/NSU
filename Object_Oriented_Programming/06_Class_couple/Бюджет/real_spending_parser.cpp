c29d7bf680ac0717ee2637adb8ce3732731212d9
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-03-02 10:50:27 +0700

    Create real_spending_parser.cpp

---
+++ Object_Oriented_Programming/06_Class_couple/Бюджет/real_spending_parser.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <sstream>
+#include <utility>
+
+#include "real_spending_parser.hpp"
+#include "spending.hpp"
+#include "date.hpp"
+
+std::pair<Spending, bool> RealSpendingParser::getNextItem() {
+    std::string line, str_path;
+    double value;
+    std::string strdate;
+    while (std::getline(is, line)) {
+        std::istringstream iss(line);
+        if (!(iss >> strdate))
+            continue;
+        if (!(iss >> str_path))
+            continue;
+        if (!(iss >> value))
+            continue;
+        std::pair<std::string, std::string> path = getPath(str_path);
+        Date date{strdate};
+        return std::make_pair(Spending(date, path.first, path.second, value), true);
+    }
+    return std::make_pair(Spending(), false);
+}
+
+std::pair<std::string, std::string> RealSpendingParser::getPath(const std::string& str) {
+    if (str.empty()) {
+        throw std::invalid_argument("Can't get path: empty string");
+    }
+    std::pair<std::string, std::string> path;
+    size_t idx = str.find(':');
+    if (idx != std::string::npos) {
+        path = std::make_pair(str.substr(0, idx), str.substr(idx + 1, str.length()));
+    } else {
+        path = std::make_pair(str.substr(0, str.length()), "");
+    }
+    return path;
+}
