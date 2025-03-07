3ffbfe12ca9ffae58def92b53e793e47d3fc3bf2
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-03-07 23:37:31 +0700

    Update secant.cpp

--- Object_Oriented_Programming/10_Design_patterns/Численное решение уравнения f(x)=0/secant.cpp
+++ Object_Oriented_Programming/10_Design_patterns/Численное решение уравнения f(x)=0/secant.cpp
@@ -1,27 +1,27 @@
 #include "secant.hpp"
 
 double Secant::execute(std::function<double(double)> function, double x1, double x2, double eps, std::function<double(double)> derivative) const {
-    double x1Initial = x1;
-    double x2Initial = x2;
+    double x1_ = x1;
+    double x2_ = x2;
 
-    double fx1 = function(x1);
-    double fx2 = function(x2);
+    double Fx1 = function(x1);
+    double Fx2 = function(x2);
 
-    if (std::abs(fx1) < eps) { return x1; }
-    if (std::abs(fx2) < eps) { return x2; }
+    if (abs(Fx1) < eps) { return x1; }
+    if (abs(Fx2) < eps) { return x2; }
 
     for (size_t i = 0; i < max_iter; i++) {
-        x1 = x2 - (x2 - x1) * fx2 / (fx2 - fx1);
-        fx1 = function(x1);
-        x2 = x1 - (x1 - x2) * fx1 / (fx1 - fx2);
-        fx2 = function(x2);
+        x1 = x2 - (x2 - x1) * Fx2 / (Fx2 - Fx1);
+        Fx1 = function(x1);
+        x2 = x1 - (x1 - x2) * Fx1 / (Fx1 - Fx2);
+        Fx2 = function(x2);
 
         if (std::isnan(x1) || std::isnan(x2)) {
             return nan("");
         }
 
         if (std::abs(x2 - x1) < eps) {
-            if (x2 < x1Initial || x2 > x2Initial) {
+            if (x2 < x1_ || x2 > x2_) {
                 return nan("");
             }
             return x2;
