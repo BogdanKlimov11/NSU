61a6f2c408c3abc1483e916d9c6781457884177c
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-05-09 18:53:19 +0700

    Update Task_2.sql

--- Databases_SQL/Task_2.sql
+++ Databases_SQL/Task_2.sql
@@ -1,132 +1,141 @@
--- 2-1
-SELECT login, avg(tasks.priority) AS average
-FROM users,
-     tasks
-WHERE tasks.responsible = users.login
-GROUP BY login
+-- Выборка средней приоритетности задач по ответственным (топ-3)
+SELECT u.login, 
+       AVG(t.priority) AS average
+FROM Users u
+JOIN Tasks t ON t.responsible = u.login
+GROUP BY u.login
 ORDER BY average
 LIMIT 3;
 
--- 2-2
-SELECT concat(count(tasks.sur_key), ' - ', extract(MONTH FROM date_on), ' - ', login) AS stats
-FROM users,
-     tasks
-WHERE tasks.responsible = users.login
-  AND date_on IS NOT NULL
-  AND extract(YEAR FROM date_on) = 2015
-GROUP BY users.login, extract(MONTH from date_on);
+-- Статистика задач по месяцам 2015 года для ответственных
+SELECT CONCAT(COUNT(t.sur_key), ' - ', EXTRACT(MONTH FROM t.date_on), ' - ', u.login) AS stats
+FROM Users u
+JOIN Tasks t ON t.responsible = u.login
+WHERE t.date_on IS NOT NULL
+  AND EXTRACT(YEAR FROM t.date_on) = 2015
+GROUP BY u.login, EXTRACT(MONTH FROM t.date_on);
 
--- 2-3
-SELECT login executor, sum(a.estimate - a.spent) "-", sum(b.spent - b.estimate) "+"  -- как еще один раз к task
-FROM users,
-     tasks a,
-     tasks b
-WHERE users.login = a.responsible
-  AND a.responsible = b.responsible
-  AND a.estimate > a.spent
+-- Выборка перерасхода и недооценки времени по ответственным (вариант 1)
+SELECT u.login AS executor, 
+       SUM(a.estimate - a.spent) AS under_estimation, 
+       SUM(b.spent - b.estimate) AS over_estimation
+FROM Users u
+JOIN Tasks a ON u.login = a.responsible
+JOIN Tasks b ON a.responsible = b.responsible
+WHERE a.estimate > a.spent
   AND b.spent > b.estimate
-GROUP BY login;
+GROUP BY u.login;
 
-SELECT login executor, sum(a.under) AS "-", sum(b.over) AS "+"
-FROM users,
-     (SELECT (estimate - spent) as under, responsible
-      FROM tasks
-      WHERE estimate > spent) AS a,
+-- Выборка перерасхода и недооценки времени по ответственным (вариант 2)
+SELECT u.login AS executor, 
+       SUM(a.under) AS under_estimation, 
+       SUM(b.over) AS over_estimation
+FROM Users u
+JOIN (
+    SELECT (estimate - spent) AS under, responsible
+    FROM Tasks
+    WHERE estimate > spent
+) a ON u.login = a.responsible
+JOIN (
+    SELECT (spent - estimate) AS over, responsible
+    FROM Tasks
+    WHERE spent > estimate
+) b ON a.responsible = b.responsible
+GROUP BY u.login;
 
-     (SELECT (spent - estimate) as over, responsible
-      FROM tasks
-      WHERE spent > estimate) AS b
-WHERE users.login = a.responsible
-  AND a.responsible = b.responsible
-GROUP BY login;
-
--- 2-4
+-- Выборка пар создатель-ответственный
 SELECT creator, responsible
-FROM tasks
+FROM Tasks
 GROUP BY creator, responsible;
 
--- 2-5
-SELECT login, length(login)
-FROM users
+-- Пользователь с самым длинным логином
+SELECT login, LENGTH(login) AS length
+FROM Users
 ORDER BY length DESC
 LIMIT 1;
 
--- 2-6
+-- Создание таблиц для сравнения размера строк
 DROP TABLE IF EXISTS Ch, VCh;
-CREATE TABLE Ch
-(
-  str VARCHAR(10485760)
+
+CREATE TABLE Ch (
+    str VARCHAR(10485760)
 );
-CREATE TABLE VCh
-(
-  str CHAR(10485760)
-); -- 1 MB
+
+CREATE TABLE VCh (
+    str CHAR(10485760)
+);
+
+-- Вставка тестовых данных
 INSERT INTO Ch VALUES ('YSDA');
 INSERT INTO VCh VALUES ('YSDA');
 
-SELECT sum(pg_column_size(Ch.str)), sum(pg_column_size(VCh.str))
-FROM Ch,
-     VCh;
+-- Сравнение размера строк в таблицах
+SELECT SUM(pg_column_size(Ch.str)) AS varchar_size, 
+       SUM(pg_column_size(VCh.str)) AS char_size
+FROM Ch, VCh;
 
--- 2-7
-SELECT login, max(priority)
-FROM users,
-     tasks
-WHERE tasks.responsible = users.login
-GROUP BY login, responsible;
+-- Максимальный приоритет задач по ответственным
+SELECT u.login, 
+       MAX(t.priority) AS max_priority
+FROM Users u
+JOIN Tasks t ON t.responsible = u.login
+GROUP BY u.login;
 
--- 2-8
-SELECT responsible, sum(estimate)
-FROM tasks,
-     (SELECT avg(estimate) FROM tasks) as a
-WHERE estimate > a.avg
-GROUP BY responsible, a.avg;
+-- Сумма оценок задач выше среднего по ответственным
+SELECT t.responsible, 
+       SUM(t.estimate) AS total_estimate
+FROM Tasks t
+CROSS JOIN (SELECT AVG(estimate) AS avg FROM Tasks) a
+WHERE t.estimate > a.avg
+GROUP BY t.responsible;
 
--- 2-9 -- Какой вью можно прокешировать?
+-- Создание представлений для статистики задач
 CREATE VIEW task_counter AS
-SELECT tasks.responsible,
-       count(tasks.responsible) AS amount
-FROM tasks
-GROUP BY tasks.responsible;
-
+SELECT responsible, 
+       COUNT(responsible) AS amount
+FROM Tasks
+GROUP BY responsible;
 
 CREATE VIEW task_complete AS
-SELECT responsible,
-       count(responsible) AS completed_task
-FROM tasks
+SELECT responsible, 
+       COUNT(responsible) AS completed_task
+FROM Tasks
 WHERE spent <= estimate
 GROUP BY responsible;
 
-
 CREATE VIEW task_delayed AS
-SELECT responsible,
-       count(responsible) AS delayed_task
-FROM tasks
+SELECT responsible, 
+       COUNT(responsible) AS delayed_task
+FROM Tasks
 WHERE spent > estimate
 GROUP BY responsible;
--- --------------
--- Не догнал, что хотят, кучу представлений или одно большое.
--- Если второе - то как?
--------------------------
 
---2-10
+-- Объединённое представление для всей статистики
+CREATE VIEW task_stats AS
+SELECT t.responsible, 
+       COALESCE(COUNT(t.responsible), 0) AS total_tasks,
+       COALESCE(SUM(CASE WHEN t.spent <= t.estimate THEN 1 ELSE 0 END), 0) AS completed_tasks,
+       COALESCE(SUM(CASE WHEN t.spent > t.estimate THEN 1 ELSE 0 END), 0) AS delayed_tasks
+FROM Tasks t
+GROUP BY t.responsible;
 
-SELECT tasks.header, users.login
-FROM users,
-     tasks
-WHERE users.login = tasks.creator;
+-- Различные варианты выборки заголовков задач и их создателей
+-- Вариант 1: Простое соединение
+SELECT t.header, u.login
+FROM Users u
+JOIN Tasks t ON u.login = t.creator;
 
-----------------------------------
+-- Вариант 2: С подзапросами
+SELECT t.header, u.login
+FROM (SELECT header, creator FROM Tasks) t
+JOIN (SELECT login FROM Users) u ON u.login = t.creator;
 
-SELECT tasks.header, users.login
-FROM (SELECT header, creator FROM tasks) AS tasks,
-     (SELECT login FROM users) AS users
-WHERE users.login = tasks.creator;
+-- Вариант 3: С подзапросом в SELECT
+SELECT t.header, 
+       (SELECT login FROM Users u WHERE u.login = t.creator) AS login
+FROM Tasks t;
 
-------------------------
-SELECT tasks.header, (SELECT login FROM users WHERE login = tasks.creator)
-FROM tasks;
-
-SELECT header, creator FROM tasks
-WHERE creator IN (SELECT login FROM users WHERE login = tasks.creator);
-------------------------
+-- Вариант 4: С IN и подзапросом
+SELECT header, creator
+FROM Tasks
+WHERE creator IN (SELECT login FROM Users WHERE login = creator);
