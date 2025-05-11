54ee84111532ca3a28e258adbf58e74ac815678a
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-05-11 13:25:34 +0700

    Update Main.py

--- Databases_SQL/Extra_task/Main.py
+++ Databases_SQL/Extra_task/Main.py
@@ -1,19 +1,38 @@
-from DB.DBcontroller import DBcontroller, Role
+from db_controller import DBController, Role
+
 
 class User:
-    def __init__(self, role, login, password, firstName=None, secondName=None, phone=None, **kwargs):
+    def __init__(self, role, login, password, first_name=None, last_name=None, phone=None, **kwargs):
         self.role = role
         self.login = login
         self.password = password
-        self.firstName = firstName
-        self.secondName = secondName
+        self.first_name = first_name
+        self.last_name = last_name
         self.phone = phone
 
 
 if __name__ == "__main__":
-    ddyak = User(role=Role.courier, login='ddyak', password='hello', firstName='Che', secondName='Lal', phone='8')
-    db = DBcontroller()
+    # Создание пользователя-курьера
+    ddyak = User(
+        role=Role.courier,
+        login="ddyak",
+        password="hello",
+        first_name="Che",
+        last_name="Lal",
+        phone="8"
+    )
+    
+    # Инициализация контроллера базы данных
+    db = DBController()
+    
+    # Регистрация пользователя
     db.register_user(ddyak)
-    db.authentication(ddyak)
-    db.add_address(14, 'Pirogova')
+    
+    # Аутентификация пользователя
+    db.authenticate_user(ddyak)
+    
+    # Добавление адреса
+    db.add_address(street="Pirogova", number=14)
+    
+    # Получение всех клиентов
     db.get_all_users(Role.client)
