c18fd0c7b338bc47109e1c470894cca1f86e0fdc
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-05-11 12:59:06 +0700

    Update Task_5-5.sql

--- Databases_SQL/Task_5-5.sql
+++ Databases_SQL/Task_5-5.sql
@@ -1,5 +1,7 @@
+-- Удаление таблиц, если они существуют
 DROP TABLE IF EXISTS _Users, _Tasks, _Projects CASCADE;
 
+-- Создание таблицы пользователей
 CREATE TABLE _Users
 (
     name       VARCHAR(32) NOT NULL,
@@ -9,6 +11,7 @@ CREATE TABLE _Users
     PRIMARY KEY (login)
 );
 
+-- Создание таблицы проектов
 CREATE TABLE _Projects
 (
     name        VARCHAR(32),
@@ -18,18 +21,19 @@ CREATE TABLE _Projects
     PRIMARY KEY (name)
 );
 
+-- Создание таблицы задач
 CREATE TABLE _Tasks
 (
     project     VARCHAR(32),
-    header      VARCHAR(32)                                                               NOT NULL,
-    priority    INTEGER                                                                   NOT NULL,
+    header      VARCHAR(32) NOT NULL,
+    priority    INTEGER NOT NULL,
     description TEXT,
     status      VARCHAR(16) CHECK (status in ('new', 'reopened', 'closed', 'in process')) NOT NULL,
     estimate    INTEGER,
     spent       INTEGER,
     creator     VARCHAR(32),
     responsible VARCHAR(32),
-    sur_key     SERIAL, -- increment : fixed
+    sur_key     SERIAL, -- Автоинкрементный ключ
     date_on     DATE,
     FOREIGN KEY (project) REFERENCES _Projects (name),
     FOREIGN KEY (creator) REFERENCES _Users (login),
@@ -37,7 +41,7 @@ CREATE TABLE _Tasks
     PRIMARY KEY (sur_key)
 );
 
--- 2
+-- Вставка данных в таблицу пользователей
 INSERT INTO _Users(name, login, email, department)
 VALUES ('Касаткин Артем', 'a.kasatkin', 'a.kasatkin@ya.ru', 'Administration'),
        ('Петрова София', 's.petrova', 's.petrova@ya.ru', 'Accounting'),
@@ -47,6 +51,7 @@ VALUES ('Касаткин Артем', 'a.kasatkin', 'a.kasatkin@ya.ru', 'Admini
        ('Белова Вера', 'v.belova', 'v.belova@ya.ru', 'Support'),
        ('Макенрой Алексей', 'a.makenroy', 'a.makenroy@ya.ru', 'Administration');
 
+-- Вставка данных в таблицу проектов
 INSERT INTO _Projects(name, date_on, date_off)
 VALUES ('РТК', '2016/01/31', NULL),
        ('СС.Контент', '2015/02/23', '2016/12/31'),
@@ -54,6 +59,7 @@ VALUES ('РТК', '2016/01/31', NULL),
        ('МВД-Онлайн', '2015/05/22', '2016/01/31'),
        ('Поддержка', '2016/06/07', NULL);
 
+-- Вставка данных в таблицу задач
 INSERT INTO _Tasks(project, header, priority, status, creator, responsible, date_on, estimate, spent)
 VALUES ('Поддержка', 'Task1', 12, 'new', 'a.berkut', 's.petrova', NULL, 10, 15),
        ('Демо-Сибирь', 'Task2', 228, 'new', 'v.belova', 's.petrova', NULL, 52, 22),
@@ -71,15 +77,18 @@ VALUES ('Поддержка', 'Task1', 12, 'new', 'a.berkut', 's.petrova', NULL,
        ('СС.Контент', 'Task14', 20, 'new', 'a.makenroy', 'a.kasatkin', '2015/8/3', 22, 3),
        ('СС.Контент', 'Task15', 20, 'new', 'a.makenroy', NULL, '2015/12/3', NULL, NULL);
 
-select * from _Tasks;
+-- Выборка всех задач
+SELECT * FROM _Tasks;
 
+-- Удаление таблицы кэша задач, если существует
 DROP TABLE IF EXISTS _Tasks_cache CASCADE;
 
+-- Создание таблицы кэша задач
 CREATE TABLE _Tasks_cache
 (
     sur_key     SERIAL,
     t_id        INT,
-    time_change TIMESTAMP   NOT NULL,
+    time_change TIMESTAMP NOT NULL,
     exists      BOOLEAN,
     project     VARCHAR(64) NOT NULL,
     header      VARCHAR(64),
@@ -97,10 +106,11 @@ CREATE TABLE _Tasks_cache
     PRIMARY KEY (sur_key)
 );
 
---********************************************************************************
+-- Удаление триггера и функции модификации, если существуют
 DROP TRIGGER IF EXISTS modification_trigger ON _Tasks;
 DROP FUNCTION IF EXISTS modification() CASCADE;
 
+-- Создание функции для логирования изменений в таблице задач
 CREATE OR REPLACE FUNCTION modification() RETURNS TRIGGER AS
 $$
 BEGIN
@@ -126,31 +136,31 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- Создание триггера для вызова функции модификации
 CREATE TRIGGER modification_trigger
     AFTER INSERT OR UPDATE OR DELETE
     ON _Tasks
     FOR EACH ROW
 EXECUTE PROCEDURE modification();
 
-
+-- Вставка тестовых задач
 INSERT INTO _Tasks(project, header, priority, status, creator, responsible, date_on, estimate, spent)
 VALUES ('Поддержка', 'Task122', 12, 'new', 'a.berkut', 's.petrova', NULL, 10, 15),
        ('Демо-Сибирь', 'Task22', 228, 'new', 'v.belova', 's.petrova', NULL, 52, 22);
 
-DELETE
-FROM _Tasks
-WHERE priority = 228;
+-- Удаление задачи с приоритетом 228
+DELETE FROM _Tasks WHERE priority = 228;
 
-SELECT *
-FROM _Tasks;
+-- Выборка всех задач
+SELECT * FROM _Tasks;
 
-SELECT *
-FROM _Tasks_cache;
+-- Выборка всех записей из кэша задач
+SELECT * FROM _Tasks_cache;
 
--- *****************************     HISTORY      ***********************************
-
+-- Удаление функции истории задач, если существует
 DROP FUNCTION IF EXISTS task_history(p VARCHAR, h VARCHAR);
 
+-- Создание функции для получения истории изменений задачи
 CREATE OR REPLACE FUNCTION task_history(p VARCHAR(256), h VARCHAR(128)) RETURNS SETOF _Tasks_cache AS
 $$
 BEGIN
@@ -159,24 +169,23 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-UPDATE _Tasks
-SET priority=17
-WHERE header = 'Task122';
+-- Обновление приоритета задачи
+UPDATE _Tasks SET priority=17 WHERE header = 'Task122';
 
-SELECT *
-FROM task_history('Поддержка', 'Task122');
+-- Получение истории изменений для задачи
+SELECT * FROM task_history('Поддержка', 'Task122');
 
--- *****************************   REVERT_CHANGE    ***************************************
-
+-- Удаление функции отката изменений, если существует
 DROP FUNCTION IF EXISTS revert_change(rid INT);
 
+-- Создание функции для отката изменений задачи
 CREATE OR REPLACE FUNCTION revert_change(rid INT) RETURNS VARCHAR AS
 $$
 DECLARE
     new _Tasks_cache%ROWTYPE;
     old _Tasks_cache%ROWTYPE;
 BEGIN
-    SELECT * FROM _Tasks_cache --WHERE t_id = rid INTO new;
+    SELECT * FROM _Tasks_cache
     WHERE sur_key = (SELECT MAX(sur_key) FROM _Tasks_cache AS int WHERE t_id = rid) INTO new;
     SELECT *
     FROM _Tasks_cache
@@ -224,40 +233,35 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT *
-FROM _Tasks_cache;
+-- Выборка записей из кэша задач
+SELECT * FROM _Tasks_cache;
 
-UPDATE _Tasks
-SET priority=99
-WHERE header = 'Task122';
+-- Обновление приоритета задачи
+UPDATE _Tasks SET priority=99 WHERE header = 'Task122';
 
-SELECT *
-FROM _Tasks_cache;
+-- Выборка записей из кэша задач
+SELECT * FROM _Tasks_cache;
 
-SELECT *
-FROM revert_change(16);
+-- Выполнение отката изменений для задачи
+SELECT * FROM revert_change(16);
 
-SELECT *
-FROM _Tasks_cache;
+-- Выборка записей из кэша задач
+SELECT * FROM _Tasks_cache;
 
-SELECT *
-FROM _Tasks;
+-- Выборка всех задач
+SELECT * FROM _Tasks;
 
-------------------------------
+-- Удаление задачи
+DELETE FROM _Tasks WHERE header = 'Task122';
 
-DELETE
-FROM _Tasks
-WHERE header = 'Task122';
+-- Выборка всех задач
+SELECT * FROM _Tasks;
 
-select *
-from _Tasks;
+-- Выборка записей из кэша задач
+SELECT * FROM _Tasks_cache;
 
-Select *
-from _Tasks_cache;
+-- Выполнение отката изменений для задачи
+SELECT * FROM revert_change(16);
 
-SELECT *
-FROM revert_change(16);
-
-select *
-from _Tasks;
-
+-- Выборка всех задач
+SELECT * FROM _Tasks;
