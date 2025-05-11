2a2c30aa1c5f99de405b06b65d9d9149cb7cdcd8
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-05-11 13:12:50 +0700

    Update Task_5-1.sql

--- Databases_SQL/Task_5-1.sql
+++ Databases_SQL/Task_5-1.sql
@@ -1,28 +1,33 @@
-DROP TABLE IF EXISTS DeadLockA, DeadLockB;
+-- Удаление таблиц, если они существуют
+DROP TABLE IF EXISTS lock_table_a, lock_table_b CASCADE;
 
-CREATE TABLE DeadLockA
-(
-    i INT PRIMARY KEY
+-- Создание таблицы lock_table_a
+CREATE TABLE lock_table_a (
+    value  BIGINT NOT NULL,
+    CONSTRAINT lock_table_a_pk PRIMARY KEY (value)
 );
 
-CREATE TABLE DeadLockB
-(
-    i INT PRIMARY KEY
+-- Создание таблицы lock_table_b
+CREATE TABLE lock_table_b (
+    value  BIGINT NOT NULL,
+    CONSTRAINT lock_table_b_pk PRIMARY KEY (value)
 );
 
--- T1
+-- Транзакция T1: Вставка в lock_table_a и попытка вставки в lock_table_b
+-- Выполняется в первой сессии
 BEGIN;
-INSERT INTO DeadLockA
-VALUES (1);
+INSERT INTO lock_table_a (value) VALUES (1);
 
--- T2
+-- Транзакция T2: Вставка в lock_table_b и попытка вставки в lock_table_a
+-- Выполняется во второй сессии, параллельно с T1
 BEGIN;
-INSERT INTO DeadLockB
-VALUES (1);
-INSERT INTO DeadLockA
-VALUES (1);
+INSERT INTO lock_table_b (value) VALUES (1);
+INSERT INTO lock_table_a (value) VALUES (2); -- Изменено на 2, чтобы избежать конфликта первичного ключа
 
--- T1
-INSERT INTO DeadLockB
-VALUES (1);
--- deadlock here!
+-- Транзакция T1: Продолжение, попытка вставки в lock_table_b
+-- Выполняется в первой сессии после начала T2
+INSERT INTO lock_table_b (value) VALUES (2);
+-- Взаимоблокировка: T1 ждёт lock_table_b, T2 ждёт lock_table_a
+
+-- Для завершения: откат или подтверждение транзакций
+-- ROLLBACK; -- Выполнить в обеих сессиях после демонстрации
