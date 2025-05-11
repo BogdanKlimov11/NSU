acfa2e3ba9f198be6730513474d0a13d96875983
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-05-11 13:14:07 +0700

    Update Task_4.sql

--- Databases_SQL/Task_4.sql
+++ Databases_SQL/Task_4.sql
@@ -1,113 +1,132 @@
---4-1
+-- Удаление таблиц, если они существуют
+DROP TABLE IF EXISTS table_a, table_b CASCADE;
 
-DROP TABLE IF EXISTS a, b;
-
-CREATE TABLE a
-(
-    common INT,
-    a_data VARCHAR(100)
+-- Создание таблицы table_a
+CREATE TABLE table_a (
+    shared_id  BIGINT NOT NULL,
+    data       VARCHAR(100),
+    CONSTRAINT table_a_pk PRIMARY KEY (shared_id)
 );
 
-CREATE TABLE b
-(
-    common INT,
-    b_data VARCHAR(100)
+-- Создание таблицы table_b
+CREATE TABLE table_b (
+    shared_id  BIGINT NOT NULL,
+    data       VARCHAR(100),
+    CONSTRAINT table_b_pk PRIMARY KEY (shared_id)
 );
 
-INSERT INTO a (common, a_data)
-VALUES (1, 'a_data1'),
-       (2, 'a_data2');
+-- Вставка тестовых данных в table_a
+INSERT INTO table_a (shared_id, data)
+VALUES 
+    (1, 'a_data1'),
+    (2, 'a_data2');
 
-INSERT INTO b (common, b_data)
-VALUES (1, 'b_data1'),
-       (3, 'b_data2'),
-       (1, 'b_data3');
+-- Вставка тестовых данных в table_b
+INSERT INTO table_b (shared_id, data)
+VALUES 
+    (1, 'b_data1'),
+    (3, 'b_data2'),
+    (1, 'b_data3');
 
-SELECT *
-FROM a
-         FULL OUTER JOIN b ON a.common = b.common
-WHERE a.common IS NULL
-   OR b.common IS NULL;
+-- 4-1: Демонстрация различных типов JOIN
 
-SELECT *
-FROM a
-         FULL OUTER JOIN b ON a.common = b.common;
+-- Запрос 1: Несоответствующие записи (только в table_a или table_b)
+SELECT a.shared_id AS a_shared_id, a.data AS a_data, b.shared_id AS b_shared_id, b.data AS b_data
+FROM table_a a
+FULL OUTER JOIN table_b b ON a.shared_id = b.shared_id
+WHERE a.shared_id IS NULL OR b.shared_id IS NULL;
 
-SELECT *
-FROM a
-         INNER JOIN b ON a.common = b.common;
+-- Запрос 2: Полное соединение (все записи)
+SELECT a.shared_id AS a_shared_id, a.data AS a_data, b.shared_id AS b_shared_id, b.data AS b_data
+FROM table_a a
+FULL OUTER JOIN table_b b ON a.shared_id = b.shared_id;
 
-SELECT *
-FROM a
-         LEFT JOIN b ON a.common = b.common;
+-- Запрос 3: Внутреннее соединение (общие записи)
+SELECT a.shared_id AS a_shared_id, a.data AS a_data, b.shared_id AS b_shared_id, b.data AS b_data
+FROM table_a a
+INNER JOIN table_b b ON a.shared_id = b.shared_id;
 
-SELECT *
-FROM a
-         LEFT JOIN b ON a.common = b.common
-WHERE b.common IS NULL;
+-- Запрос 4: Левое соединение (все записи из table_a)
+SELECT a.shared_id AS a_shared_id, a.data AS a_data, b.shared_id AS b_shared_id, b.data AS b_data
+FROM table_a a
+LEFT JOIN table_b b ON a.shared_id = b.shared_id;
 
-SELECT *
-FROM a
-         RIGHT JOIN b ON a.common = b.common;
+-- Запрос 5: Записи только в table_a (без соответствия в table_b)
+SELECT a.shared_id AS a_shared_id, a.data AS a_data, b.shared_id AS b_shared_id, b.data AS b_data
+FROM table_a a
+LEFT JOIN table_b b ON a.shared_id = b.shared_id
+WHERE b.shared_id IS NULL;
 
-SELECT *
-FROM a
-         RIGHT JOIN b ON a.common = b.common
-WHERE a.common IS NULL;
+-- Запрос 6: Правое соединение (все записи из table_b)
+SELECT a.shared_id AS a_shared_id, a.data AS a_data, b.shared_id AS b_shared_id, b.data AS b_data
+FROM table_a a
+RIGHT JOIN table_b b ON a.shared_id = b.shared_id;
 
+-- Запрос 7: Записи только в table_b (без соответствия в table_a)
+SELECT a.shared_id AS a_shared_id, a.data AS a_data, b.shared_id AS b_shared_id, b.data AS b_data
+FROM table_a a
+RIGHT JOIN table_b b ON a.shared_id = b.shared_id
+WHERE a.shared_id IS NULL;
 
--- 4-2
+-- 4-2: Поиск задач с максимальным приоритетом для каждого создателя
 
-SELECT sur_key, header
-FROM tasks AS out
-WHERE priority = (SELECT MAX(priority) FROM tasks AS int WHERE int.creator = out.creator);
+-- Вариант 1: Использование подзапроса
+SELECT id, header
+FROM tasks t1
+WHERE priority = (
+    SELECT MAX(priority)
+    FROM tasks t2
+    WHERE t2.creator = t1.creator
+);
 
-SELECT b.sur_key, b.header
-FROM tasks AS a,
-     tasks AS b
-WHERE a.creator = b.creator
-GROUP BY b.sur_key, a.creator
-HAVING max(a.priority) = b.priority;
+-- Вариант 2: Использование оконной функции (оптимизированный)
+SELECT id, header
+FROM (
+    SELECT id, header, priority,
+           MAX(priority) OVER (PARTITION BY creator) AS max_priority
+    FROM tasks
+) ranked
+WHERE priority = max_priority;
 
+-- 4-3: Поиск пользователей, не ответственных за задачи
 
--- 4-3
-
-SELECT login
-FROM users
-WHERE login NOT IN (SELECT responsible FROM tasks WHERE responsible IS NOT NULL);
-
-SELECT DISTINCT login
-FROM users
-         LEFT OUTER JOIN tasks AS t ON users.login = t.responsible
+-- Вариант 1: Использование LEFT JOIN (оптимизированный)
+SELECT u.login
+FROM users u
+LEFT JOIN tasks t ON u.login = t.responsible
 WHERE t.responsible IS NULL;
 
-SELECT DISTINCT u.login
-FROM users AS u
-WHERE (SELECT DISTINCT responsible FROM tasks AS t WHERE t.responsible = u.login AND responsible IS NOT NULL
-      ) IS NULL;
+-- Вариант 2: Использование NOT EXISTS
+SELECT u.login
+FROM users u
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM tasks t
+    WHERE t.responsible = u.login
+);
 
-
--- 4-4
+-- 4-4: Объединение пар responsible и creator
 
 SELECT responsible, creator
 FROM tasks
 WHERE responsible IS NOT NULL
-UNION
+UNION ALL
 SELECT responsible, creator
 FROM tasks
 WHERE responsible IS NOT NULL;
 
+-- 4-5: Демонстрация CROSS JOIN
 
--- 4-5) origin and 2 synonyms
+-- Вариант 1: Явный CROSS JOIN
+SELECT p.name AS project_name, t.header AS task_header
+FROM tasks t
+CROSS JOIN projects p;
 
-SELECT p.name, t.header
-FROM tasks as t
-         CROSS JOIN projects as p;
+-- Вариант 2: JOIN с условием TRUE
+SELECT p.name AS project_name, t.header AS task_header
+FROM tasks t
+JOIN projects p ON TRUE;
 
-SELECT p.name, t.header
-FROM tasks as t
-         JOIN projects as p ON TRUE;
-
-SELECT p.name, t.header
-FROM tasks as t,
-     projects as p;
+-- Вариант 3: Устаревший синтаксис (для демонстрации)
+SELECT p.name AS project_name, t.header AS task_header
+FROM tasks t, projects p;
