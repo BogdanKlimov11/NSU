ba6179416d1d1bb4f63b58c30ae80f2937a1aedf
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-05-11 14:21:01 +0700

    Delete Object_Oriented_Programming/05_One_class directory

--- Object_Oriented_Programming/05_One_class/Рекомендация френдов/writer.cpp
+++
@@ -1,9 +0,0 @@
-#include "writer.hpp"
-
-void Writer::writeRecommendedFriends(const std::vector<std::string>& friends) {
-    if (friends.empty()) return;
-    os << "Recommendation for " << friends[0] << '\n';
-    for (size_t i = 1; i < friends.size(); i++) {
-        os << friends[i] << '\n';
-    }
-}
