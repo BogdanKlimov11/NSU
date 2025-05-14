c584afff5ef3fcd0f01176ea1320446608f9e91f
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-05-14 18:40:14 +0700

    Update client.py

--- Networks/ZeroMQ/client.py
+++ Networks/ZeroMQ/client.py
@@ -6,7 +6,7 @@ context = zmq.Context()
 # socket to talk to server
 print("Connecting to server…")
 socket = context.socket(zmq.REQ)
-n=raw_input('enter the number of running servers > ')
+n = raw_input('enter the number of running servers > ')
 
 for i in range(1, int(n) + 1):
     socket.connect("tcp://localhost:555" + str(i))
