945ca7209e873b356a591397e953a71bc655dca8
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-05-14 18:20:26 +0700

    Update server.py

--- Networks/Simple_proxy/server.py
+++ Networks/Simple_proxy/server.py
@@ -1,14 +1,17 @@
 import socket
 import time
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('localhost', 5556))
 sock.listen(10)
 srvmsg = 'World!'
+
 while True:
     conn, addr = sock.accept()
     data = conn.recv(1024)
     if not data:
         break
+    
     print('received message: ' + data.decode())
     #time.sleep(2)
     data = data + srvmsg.encode()
