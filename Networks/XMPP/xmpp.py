2ff7a07ae45f309fbb719ef5656e4237b4209fad
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-05-14 18:37:09 +0700

    Update xmpp.py

--- Networks/XMPP/xmpp.py
+++ Networks/XMPP/xmpp.py
@@ -3,11 +3,9 @@ import requests
 from xml.etree import ElementTree
 import time
 
-ADDR = 'kirillacctest@xmpp.jp'
-
+ADDR = 'mynewlogin@xmpp.jp'
 
 class Client(ClientXMPP):
-
     def __init__(self, jid, password):
         ClientXMPP.__init__(self, jid, password)
 
@@ -22,20 +20,22 @@ class Client(ClientXMPP):
         if msg['type'] in ('chat', 'normal'):
             msg.reply("U r so lonely that u r writing to bot?").send()
 
-
-xmpp = Client('tstccnt@xmpp.jp', '12345678')
+xmpp = Client('mynewbot@xmpp.jp', '12345678')
 xmpp.connect()
 xmpp.process()
 s = requests.session()
 last = None
+
 while True:
     r = s.get('http://www.moex.com/iss/engines/currency/markets/selt/boards/CETS/securities/EUR_RUB__TOM.xml?iss.meta=off&iss.only=marketdata')
     now = ElementTree.fromstring(r.text)[0][0][0].attrib['LAST']
     print("now: " + now)
+    
     if (last == None):
         last = now
     else:
         if (last != now):
             last = now
             xmpp.send_message(mto=ADDR, mbody=('New EUR/RUB rate at MOEX: %s' % (last,)), mtype=('chat'))
+    
     time.sleep(1)
