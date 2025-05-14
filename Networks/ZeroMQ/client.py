29e8bb33fbdbb68c7581c05949d07fa1c8aadeb8
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-05-14 18:39:27 +0700

    Update client.py

--- Networks/ZeroMQ/client.py
+++ Networks/ZeroMQ/client.py
@@ -8,13 +8,13 @@ print("Connecting to server…")
 socket = context.socket(zmq.REQ)
 n=raw_input('enter the number of running servers > ')
 
-for i in range(1, int(n)+1):
-    socket.connect("tcp://localhost:555"+str(i))
+for i in range(1, int(n) + 1):
+    socket.connect("tcp://localhost:555" + str(i))
 
 # do 10 requests, waiting each time for a response
 for request in range(10):
     print("Sending request %s …" % request)
-    socket.send(str.encode('message'+str(request)))
+    socket.send(str.encode('message' + str(request)))
 
     # get the reply.
     message = socket.recv()
