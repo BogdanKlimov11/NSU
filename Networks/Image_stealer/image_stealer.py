9d03d7d6e2343b4c18e0ab3218d4d5dd05a844e8
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-05-14 18:16:41 +0700

    Update image_stealer.py

--- Networks/Image_stealer/image_stealer.py
+++ Networks/Image_stealer/image_stealer.py
@@ -2,16 +2,14 @@ from scapy import sendrecv
 import time
 
 # steals PNG, GIF and JPEG image if it is not fragmented
-
-
 def small_stealer(packet):
     packet_bytes = bytes(packet)
     begin = packet_bytes.find('\x89PNG')
     ### PNG ###
-    if begin > -1:  # if found PNG beginning
+    if begin > -1: # if found PNG beginning
         print 'Found \'\x89PNG\' at', begin
         end = packet_bytes.find('IEND')
-        if end > -1:  # if found PNG end
+        if end > -1: # if found PNG end
             print'---Found \'IEND\' at', end
             file_name = str(time.time()) + '.png'
             with open(file_name, 'wb') as image_file:
@@ -20,10 +18,10 @@ def small_stealer(packet):
                 image_file.close()
     ### GIF ###
     begin = packet_bytes.find('GIF8')
-    if begin > -1:  # if found GIF beginning
+    if begin > -1: # if found GIF beginning
         print 'Found \'GIF8\' at', begin
         end = packet_bytes.find(';')
-        if end > -1:  # if found GIF end
+        if end > -1: # if found GIF end
             print '---Found \';\' at', end
             file_name = str(time.time()) + '.gif'
             with open(file_name, 'wb') as image_file:
@@ -32,10 +30,10 @@ def small_stealer(packet):
                 image_file.close()
     ### JPEG ###
     begin = packet_bytes.find(chr(0xFF) + chr(0xD8))
-    if begin > -1:  # if found JPEG beginning
+    if begin > -1: # if found JPEG beginning
         print 'Found 0xFFD8 at', begin
         end = packet_bytes.find(chr(0xFF) + chr(0xD9))
-        if end > -1:  # if found JPEG end
+        if end > -1: # if found JPEG end
             print '---Found 0xFFD9 at', end
             file_name = str(time.time()) + '.jpg'
             with open(file_name, 'wb') as image_file:
@@ -43,6 +41,5 @@ def small_stealer(packet):
                 image_file.write(packet_bytes[begin:])
                 image_file.close()
 
-
 print 'Started sniffing...'
-sendrecv.sniff(prn=small_stealer)
+sendrecv.sniff(prn = small_stealer)
