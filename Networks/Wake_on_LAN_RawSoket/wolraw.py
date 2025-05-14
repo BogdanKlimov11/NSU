0e6549bcf3f355e7b9a02b99dc9fbce6be51daac
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-05-14 18:28:29 +0700

    Update wolraw.py

--- Networks/Wake_on_LAN_RawSoket/wolraw.py
+++ Networks/Wake_on_LAN_RawSoket/wolraw.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import socket
 import sys
 import uuid
@@ -10,10 +11,9 @@ mac1 = '7824AFD8902F'
 #mac1 = 'E89D87DE0B28';
 mac0 = 'B88D12560DBC'
 
-
 magic = bytes(bytearray.fromhex(''.join(['FFFFFFFFFFFF', mac1 * 16])))
-packet = bytes(bytearray.fromhex(''.join([mac1, mac0, TYPE])))+magic
+packet = bytes(bytearray.fromhex(''.join([mac1, mac0, TYPE]))) + magic
 
-sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)    #AF_PACKET
+sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
 sock.bind(('eno1',0))
 sock.send(packet)
