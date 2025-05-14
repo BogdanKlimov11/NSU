4e46665e9433e48a7095714c1c8b564a5fd3bc50
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-05-14 18:25:41 +0700

    Update UDP_voice_chat.py

--- Networks/Voice_chat/UDP_voice_chat.py
+++ Networks/Voice_chat/UDP_voice_chat.py
@@ -9,16 +9,16 @@ CHANNELS = 1
 RATE = 44100
 RECORD_SECONDS = 0.5
 
-
 def sender(socket):
-    FORMAT = pyaudio.paInt16    # initializing variables for recording vocie
+    FORMAT = pyaudio.paInt16 # initializing variables for recording vocie
     p = pyaudio.PyAudio()
 
-    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+    stream = p.open(format = FORMAT, channels = CHANNELS, rate = RATE, input = True, frames_per_buffer = CHUNK)
 
-    def psize(size):    # method needed for compressing message
+    def psize(size): # method needed for compressing message
         buff = b''
         mask = 0
+        
         while size > 0:
             buff = bytes([(size % 2 ** 7) | mask]) + buff
             mask = 0x80
@@ -28,14 +28,16 @@ def sender(socket):
     encoder = WBEncoder()
     print('frame size: %d' % encoder.frame_size)
     count = 0
+    
     while True:
         vocoded = b''
         packet_size = encoder.frame_size * 2 * 1
-
         str_send = bytes('', 'utf-8')
-        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):  # loop "for" to record message
+        
+        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)): # loop "for" to record message
             str_send = str_send + stream.read(CHUNK)
-        for i in range(0, len(str_send), packet_size):  # loop "for" to compress message
+        
+        for i in range(0, len(str_send), packet_size): # loop "for" to compress message
             packet = str_send[i:i + packet_size]
             if len(packet) != packet_size:
                 end = len(str_send) - len(str_send) % (encoder.frame_size * 2)
@@ -49,46 +51,48 @@ def sender(socket):
         #print("message len: ", len(vocoded))
         socket.sendto(vocoded, ('255.255.255.255', 11719))
 
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    # socket for receiving
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # socket for receiving
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.bind(('0.0.0.0', 11719))
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)     # socket for sending
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # socket for sending
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-pygame.mixer.init()         # initialize pygame
-S = pygame.mixer.Sound      # S needed to play voice message
+pygame.mixer.init() # initialize pygame
+S = pygame.mixer.Sound # S needed to play voice message
 
-th_send = Thread(target=sender, args=(sock, ))
+th_send = Thread(target = sender, args = (sock, ))
 th_send.start()
 
-
-def usize(buffer):      #   methods which needed to decompress message
+def usize(buffer): # methods which needed to decompress message
     sizes = []
+    
     for char in buffer:
         sizes.append(char & 0x7f)
         if char & 0x80 == 0:
             break
 
     size = 0
+    
     for i in range(len(sizes)):
         size += sizes[i] * (2 ** 7) ** (len(sizes) - i - 1)
     return len(sizes), size
-
 
 while True:
     message, addr = s.recvfrom(4757)
     #print("this is addr: ", addr)
     #print(addr)
+    
     if addr[0] != '192.168.43.140':
         vocoded = message
         decoder = WBDecoder()
         i = 0
         pcm = b''
-        while i < len(vocoded):     # loop "while" for decompressing message
+        
+        while i < len(vocoded): # loop "while" for decompressing message
             header_size, packet_size = usize(vocoded[i:])
             pcm += decoder.decode(vocoded[i + header_size:i + header_size + packet_size])
             i += header_size + packet_size
-        S(pcm).play()       # playing message
+        
+        S(pcm).play() # playing message
