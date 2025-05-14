316c20acd0e6623c0c09d17969e834393d2b26f1
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-05-14 18:40:03 +0700

    Update server.py

--- Networks/ZeroMQ/server.py
+++ Networks/ZeroMQ/server.py
@@ -4,16 +4,16 @@ from pip._vendor.distlib.compat import raw_input
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-n=raw_input('server number > ')
+n = raw_input('server number > ')
 socket.bind("tcp://*:555"+n)
 
 while True:
-    #  Wait for next request from client
+    # wait for next request from client
     message = socket.recv()
     print("Received request: %s" % message)
 
-    #  Do some 'work'
+    # do some 'work'
     time.sleep(0.5)
 
-    #  Send reply back to client
+    # send reply back to client
     socket.send(message)
