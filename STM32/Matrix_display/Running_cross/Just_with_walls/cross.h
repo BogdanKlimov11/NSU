5f05d56a5fc842d1914338da6ed7a365180c602f
PROgerych <136115919+BogdanKlimov11@users.noreply.github.com>
2025-05-17 15:21:22 +0700

    Delete STM32/Matrix_display/Running_cross/Just_with_walls directory

--- STM32/Matrix_display/Running_cross/Just_with_walls/cross.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CROSS_H_
-#define _CROSS_H_
-
-#include "stm32f0xx.h"
-
-void paint(uint8_t start_x, uint8_t start_y, uint8_t shift);
-
-#endif
--- STM32/Matrix_display/Running_cross/Just_with_walls/matrix.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MATRIX_H_
-#define _MATRIX_H_
-
-#include "stm32f0xx.h"
-
-void matrix_init(void);
-
-#endif
