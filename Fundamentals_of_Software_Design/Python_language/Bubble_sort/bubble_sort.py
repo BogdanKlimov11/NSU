17f83ba11320a2b9144e7ed91b23f1e086ac5131
Klimov Bogdan <136115919+KeroDeep@users.noreply.github.com>
2025-07-14 00:43:09 +0700

    Create bubble_sort.py

---
+++ Fundamentals_of_Software_Design/Python_language/Bubble_sort/bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped:
+            break
